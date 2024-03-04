54d6614cb843cf6e99bcca2262020fa7b7fb87a3
Chris Pickett <chris@saplingpay.com>
2024-03-04 15:27:45 +0100

    Fix not loading issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     env: {
         SERVER_URL: process.env.SERVER_URL,
-        SERVER_AUTH_URL: process.env.SERVER_AUTH_URL,
         DB_STORAGE_URL: process.env.DB_STORAGE_URL,
     }
 };
